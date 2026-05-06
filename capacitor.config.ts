b0aa36dcad1aafa4267472c965d978ac3e154568
looksai <alanmonte36@gmail.com>
2026-05-06 17:27:40 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.looksai.bodyai',
   appName: 'BodyAI Connect',
-  webDir: 'dist/public',
+  webDir: 'www',
   bundledWebRuntime: false,
 };
 
